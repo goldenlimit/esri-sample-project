0c8b5069739005262535923e2ec20bb93e24b779
Nathan Wu <Yue_Wu@esri.com>
2016-10-06 17:06:02 -0700

    Update getBasemapAttribution function and remove Power by Esri label

--- iOS_Runtime/Esri-map-Attribution-Swift/MapViewDemo-Swift/ViewController.swift
+++ iOS_Runtime/Esri-map-Attribution-Swift/MapViewDemo-Swift/ViewController.swift
@@ -9,17 +9,18 @@
 // notice and use restrictions.
 //
 // See the use restrictions at http://help.arcgis.com/en/sdk/10.0/usageRestrictions.htm
-//
+// Developer: Nathan (Yue) Wu
 
 import UIKit
 import ArcGIS
 
-class ViewController: UIViewController, UITextViewDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var attributionLabel: UILabel!
-    @IBOutlet weak var esriURL: UITextView!
     @IBOutlet weak var mapView: AGSMapView!
     
+    private var useServiceBasemap = false
+    private var useVectorBasemap =  true
     private var tiledLayer: AGSArcGISTiledLayer!
     var map:AGSMap!
     var currentViewPoint: AGSViewpoint!
@@ -30,18 +31,70 @@ class ViewController: UIViewController, UITextViewDelegate {
     var xmax: Double!
     var scale: Double!
     
+    func createBasemapTiledLayer(URL:NSURL) -> AGSArcGISTiledLayer {
+        //create an instance of a map with requested basemap
+        let tiledLayer = AGSArcGISTiledLayer(URL:URL)
+        return tiledLayer
+    }
+    
+    func createBasemapVectorTiledLayer(URL:NSURL) -> AGSArcGISVectorTiledLayer {
+        //create an instance of map with requested vector tiled basemap
+        let tiledLayer = AGSArcGISVectorTiledLayer(URL:URL)
+        return tiledLayer
+    }
+    
+    func createBasemapFromEsri() -> AGSBasemap {
+        var basemap: AGSBasemap
+        if (useServiceBasemap) {
+            basemap = AGSBasemap.streetsBasemap()
+        } else if (useVectorBasemap) {
+            basemap = AGSBasemap(baseLayer: self.createBasemapVectorTiledLayer(NSURL(string: "http://basemaps.arcgis.com/b2/arcgis/rest/services/World_Basemap/VectorTileServer")!))
+        } else {
+            basemap = AGSBasemap(baseLayer: self.createBasemapTiledLayer(NSURL(string: "http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer")!))
+        }
+        return basemap
+    }
+    
+    //Change the logic of this func and conditionally return the right attributions for different service
+    //However one thing I found that for in our current API seems that we doesn't have any interface to get the attribution
+    //The only thing I can find is AGSVectorTileSourceInfo
+    //https://developers.arcgis.com/ios/beta/api-reference/interface_a_g_s_vector_tile_source_info.html
+    func getBasemapAttribution(vectorTile:Bool) -> String {
+            if (!vectorTile) {
+                return (self.map.basemap.baseLayers[0].mapServiceInfo!?.attribution)!
+            } else {
+                return (self.map.basemap.baseLayers[0].sourceInfo!?.name)!
+            }
+}
+    
+    func configAttributionLabel() {
+        self.attributionLabel.numberOfLines = 3
+        //self.attributionLabel.lineBreakMode = .ByTruncatingTail
+        self.attributionLabel.textColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+        self.attributionLabel.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.65)
+        self.attributionLabel.textAlignment = NSTextAlignment.Left
+    }
+    
+    func updateAttribution() {
+        //set map attribution to show once map load up
+        let attributeFont = UIFont(name: "Helvetica Neue", size: 6)
+        let mapAttributes = [NSFontAttributeName: attributeFont!]
+        let mapAttributionString = NSMutableAttributedString(string: self.getBasemapAttribution(useVectorBasemap), attributes: mapAttributes)
+        self.attributionLabel.attributedText = mapAttributionString
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //create an instance of a map with ESRI topographic basemap
         self.tiledLayer = AGSArcGISTiledLayer(URL: NSURL(string: "http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer")!)
         
-        self.map = AGSMap(basemap: AGSBasemap(baseLayer: self.tiledLayer))
+        self.map = AGSMap(basemap: self.createBasemapFromEsri())
         
         map.initialViewpoint = AGSViewpoint(center: AGSPoint(x: -13045884, y: 4036331, spatialReference: AGSSpatialReference.webMercator()), scale: 1e6)
 
         self.mapView.map = self.map
-        self.esriURL.delegate = self
+        self.configAttributionLabel()
         
         //Setup a completion handler until the map loaded then show the attribution label
         self.map.loadWithCompletion { (error) -> Void in
@@ -49,38 +102,11 @@ class ViewController: UIViewController, UITextViewDelegate {
                 print(error)
             } else {
                 
-                //set map attribution to show once map load up
-                let attributeFont = UIFont(name: "Helvetica Neue", size: 6)
-                
-                let mapAttributes = [
-                    NSFontAttributeName: attributeFont!
-                ]
-                
-                let mapAttributionString = NSMutableAttributedString(string: self.tiledLayer.mapServiceInfo!.attributionText, attributes: mapAttributes)
-                
-                self.attributionLabel.attributedText = mapAttributionString
-                self.attributionLabel.numberOfLines = 2
-                self.attributionLabel.textColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
-                self.attributionLabel.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.65)
-                self.attributionLabel.textAlignment = NSTextAlignment.Left
-                
-                //print(self.tiledLayer.mapServiceInfo!.attributionText)
-                
-                //set the Esri URL clickable URL
-                let esriAttribution = "Powered by Esri"
-                let esriAttributionString = NSMutableAttributedString(string: esriAttribution, attributes: [NSFontAttributeName:UIFont(name: "Helvetica Neue", size: 7.5)!]
-                )
-                
-                esriAttributionString.addAttribute(NSLinkAttributeName, value: "www.esri.com", range:NSMakeRange(11, 4))
-                self.esriURL.attributedText = esriAttributionString
-                self.esriURL.textColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
-                self.esriURL.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.65)
-                self.esriURL.textAlignment = NSTextAlignment.Right
-                
+                self.updateAttribution()
             }
         }
         
-        //this may not fit for performance since we may call it too often and we can't return anything back
+        //this may not good for performance since we may call it too often and we can't return anything back
         self.mapView.viewpointChangedHandler = {
             () -> Void in
             
@@ -125,14 +151,6 @@ class ViewController: UIViewController, UITextViewDelegate {
                 
             }
         }
-    }
-    
-    //MARK: - UITextViewDelegate triggerred when "Powered by Esri" TextView clicked, open Safari to open the webpage
-    func textView(textView: UITextView, shouldInteractWithURL URL: NSURL, inRange characterRange: NSRange) -> Bool {
-        self.esriURL.dataDetectorTypes = UIDataDetectorTypes.Link;
-        UIApplication.sharedApplication().openURL(NSURL(string: "https://www.esri.com")!)
-        return true
-
     }
 }
 
